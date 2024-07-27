fc0e3ee271ee7c07142cf848249c4f692d8f0ec6
Boris <megal00ki@gmail.com>
2024-07-27 15:15:00 +0200

    - Add timezone block to the MOS request. - Set time zone info during Wifi setup or during email setup.

--- app.js
+++ app.js
@@ -451,8 +451,9 @@ function setDeviceWiFiSettings(){
 
   FlamingoBle.setWiFiSSIDString(ssid)
   .then(FlamingoBle.setWiFiPWDString(pwd))
-  .then(FlamingoBle.setWiFiEnable(true))
-  
+  .then(FlamingoBle.setWiFiEnable(true));
+
+  FlamingoBle.setTZInfoDeviceString(Intl.DateTimeFormat().resolvedOptions().timeZone);
 
   setTimeout(function(){ 
       handleWiFiPWD("");
@@ -464,7 +465,9 @@ function setDeviceWiFiSettings(){
 function setDeviceInfo(){
   email = document.querySelector('#inputEmail').value;
 
-  FlamingoBle.setEmailString(email)
+  FlamingoBle.setEmailString(email);
+  FlamingoBle.setTZInfoDeviceString(Intl.DateTimeFormat().resolvedOptions().timeZone);
+
 
   setTimeout(function(){ 
       FlamingoBle.getEmailString().then(handleDeviceEmail);
@@ -592,12 +595,14 @@ document.getElementById('MOS_request').addEventListener('click', function() {
   `;
 
   const url = "https://movably.fielden.com.au/api/graphql";
+  const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   const options = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json;charset=UTF-8',
         'API-KEY': 'f3162d2f-e798-4b5e-ae59-9540d69dc56c',
         'Context-Type': 'application/json;charset=UTF-8',
+        'Time-Zone': timezone
       },
       body: JSON.stringify({ query: query })
   };
